20d84e1bc4d5cbaeeeb9420057fe9056b5b3ab8e
plaexmaster <112503083+plaexmaster@users.noreply.github.com>
2023-07-28 11:41:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+COOKIES="/gluetun/deluge_cookies.txt"
+
 update_port () {
   PORT=$(cat $PORT_FORWARDED)
   echo "Portnumber ($PORT) loaded from Gluetun..."
   FORMATTED_PORT="[$PORT, $PORT]"
   echo "Portnumber formatted to fit Deluge config: $FORMATTED_PORT"
-  rm -f "/gluetun/deluge_cookies.txt"
+  rm -f $COOKIES
   echo "Accessing Deluge WEBUI..."
-  curl -s -c /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "auth.login", "params": ["'$DELUGE_PASS'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
-  response=$(curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "web.get_hosts", "params": [], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
+  curl -s -c "'$COOKIES'" -H "Content-Type: application/json" -d '{"method": "auth.login", "params": ["'$DELUGE_PASS'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
+  response=$(curl -s -b "'$COOKIES'" -H "Content-Type: application/json" -d '{"method": "web.get_hosts", "params": [], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
   hostid=$(echo "$response" | jq -r '.result[0][0]')
   echo "HostID set to: $hostid"
-  curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "web.connect", "params": ["'$hostid'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
-  curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"random_port": false}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
-  response1=$(curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "core.get_config_value", "params": ["random_port"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
+  curl -s -b "'$COOKIES'" -H "Content-Type: application/json" -d '{"method": "web.connect", "params": ["'$hostid'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
+  curl -s -b "'$COOKIES'" -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"random_port": false}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
+  response1=$(curl -s -b "'$COOKIES'" -H "Content-Type: application/json" -d '{"method": "core.get_config_value", "params": ["random_port"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
   randomportstatus=$(echo "$response1" | jq -r '.result')
   echo "Random port changed to: $randomportstatus"
-  curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"listen_ports": "'$FORMATTED_PORT'"}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
-  response2=$(curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "core.get_config_value", "params": ["listen_ports"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
+  curl -s -b "'$COOKIES'" -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"listen_ports": "'$FORMATTED_PORT'"}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
+  response2=$(curl -s -b "'$COOKIES'" -H "Content-Type: application/json" -d '{"method": "core.get_config_value", "params": ["listen_ports"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
   listenports=$(echo "$response2" | jq -r '.result')
   echo "Ports changed to $listenports"
   rm -f "/gluetun/deluge_cookies.txt"
