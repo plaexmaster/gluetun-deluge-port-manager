6d6cfdc1d32adcb536c119fa648f56c33f64cdd7
plaexmaster <112503083+plaexmaster@users.noreply.github.com>
2023-07-28 13:00:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@
 update_port () {
   PORT=$(cat $PORT_FORWARDED)
   echo "Portnumber ($PORT) loaded from Gluetun..."
-  FORMATTED_PORT="[$PORT, $PORT]"
-  echo "Portnumber formatted to fit Deluge config: $FORMATTED_PORT"
   rm -f "/gluetun/deluge_cookies.txt"
   echo "Accessing Deluge WEBUI..."
   curl -s -c /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "auth.login", "params": ["'$DELUGE_PASS'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
@@ -13,7 +11,7 @@ update_port () {
   echo "HostID set to: $hostid"
   curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "web.connect", "params": ["'$hostid'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
   curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"random_port": false}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
-  curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"listen_ports": "'$FORMATTED_PORT'"}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
+  curl -s -b /gluetun/deluge_cookies.txt -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"listen_ports": "'[$PORT, $PORT]'"}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
   rm -f "/gluetun/deluge_cookies.txt"
 }
 
