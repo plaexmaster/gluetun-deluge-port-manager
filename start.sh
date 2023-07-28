00f3b54d98007142c96b9c4998dde9ea5e24b904
plaexmaster <112503083+plaexmaster@users.noreply.github.com>
2023-07-28 13:35:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,17 +4,17 @@ update_port () {
   PORT=$(cat $PORT_FORWARDED)
   echo "Portnumber ($PORT) loaded from Gluetun..."
   echo "Accessing Deluge WEBUI..."
-  cookie=$(curl -s -c - -H "Content-Type: application/json" -d '{"method": "auth.login", "params": ["'$DELUGE_PASS'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
-  response=$(echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "web.get_hosts", "params": [], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
+  cookie=$(curl -s -c - -H "Content-Type: application/json" -d '{"method": "auth.login", "params": ["'$DELUGE_PASS'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json) > /dev/null
+  response=$(echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "web.get_hosts", "params": [], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json) > /dev/null
   hostid=$(echo "$response" | jq -r '.result[0][0]')
   echo "HostID set to: $hostid"
-  echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "web.connect", "params": ["'$hostid'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
-  echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"random_port": false}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
-  response1=$(echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "core.get_config_value", "params": ["random_port"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
+  echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "web.connect", "params": ["'$hostid'"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json > /dev/null
+  echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"random_port": false}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json > /dev/null
+  response1=$(echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "core.get_config_value", "params": ["random_port"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json) > /dev/null
   randomportstatus=$(echo "$response1" | jq -r '.result')
   echo "Random port changed to: $randomportstatus"
-  echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"listen_ports": ["'$PORT'", "'$PORT'"]}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json
-  response2=$(echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "core.get_config_value", "params": ["listen_ports"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json)
+  echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"listen_ports": ["'$PORT'", "'$PORT'"]}], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json > /dev/null
+  response2=$(echo "$cookie" | curl -s -b - -H "Content-Type: application/json" -d '{"method": "core.get_config_value", "params": ["listen_ports"], "id": 1}' $DELUGE_SERVER:$DELUGE_PORT/json) > /dev/null
   listenports=$(echo "$response2" | jq -r '.result')
   echo "Ports changed to $listenports"
 }
